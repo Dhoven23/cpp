37aa2ebe3aea0e299f94482b03d3884390f99f93
Daniel Hoven <dhoven@my.gcu.edu>
2023-09-17 12:37:10 -0700

    Finish Cell-block compression algorithm

--- LineFunctions.h
+++ LineFunctions.h
@@ -2,6 +2,7 @@
 #include <vector>
 
 #define DEPTH 17
+#define GRIDSIZE 32
 
 // Example Vector struct
 typedef struct {
@@ -21,10 +22,32 @@ typedef struct {
 	double data_last;
 } DataVector;
 
+
+typedef struct {
+	std::vector<GridVector> GridVector;
+} GridBlockVector;
+
+void CreateGridBlockVector(std::vector<GridBlockVector>& arr, int L){
+	for (int i = 0; i < L; ++i){
+		GridBlockVector gridBlock;
+		for (int j = 0; j < DEPTH; ++j){
+			// create element
+			GridVector a;
+			a.dataAddress = (i * 16) + j;
+			a.coordinate =  (i * 16) + j;
+			// add element to vector
+			gridBlock.GridVector.push_back(a);
+		}
+		arr.push_back(gridBlock);
+	}
+}
+
+
+
+
 // Create Vector of structured memory
 void CreateGridVector(std::vector<GridVector>& arr, unsigned L) {
-	for (int i = 0; i < L; ++i)
-	{
+	for (int i = 0; i < L; ++i){
 		// create element
 		GridVector a;
 		a.dataAddress = i;
@@ -34,10 +57,11 @@ void CreateGridVector(std::vector<GridVector>& arr, unsigned L) {
 	}
 }
 
+
+
 // Create Vector of Data
 void CreateDataVector(std::vector<DataVector>& arr, int L) {
-	for (int i = 0; i < L; ++i)
-	{
+	for (int i = 0; i < L; ++i){
 		DataVector a;
 		a.data = 0;
 		arr.push_back(a);
@@ -45,71 +69,92 @@ void CreateDataVector(std::vector<DataVector>& arr, int L) {
 	}
 }
 
-void DeleteRecursion(std::vector<GridVector>& arr, std::vector<DataVector>& data, std::vector<GridVector>& outputGrid, double threshold, int step)
-{
+void DeleteRecursion(std::vector<GridVector>& arr, std::vector<DataVector>& data, std::vector<GridVector>& outputGrid, double threshold, int step, int block){
 	int index = step;
 	int count = step*2;
 
-	while(index != -1)
-	{
+	while(index != -1){
 		double last_point = data[arr[index-step].dataAddress].data;
 		double next_point = data[arr[index+step].dataAddress].data;
 		double current_point = data[arr[index].dataAddress].data;
 		double error = abs(current_point - (last_point + (next_point - last_point)/2));
 		
-		if ( error > threshold)
-		{
-			outputGrid[index] = arr[index];
-
+		if ( error > threshold){
+			outputGrid[index].dataAddress = arr[index].dataAddress;
+			outputGrid[index].coordinate += (16 * block);
 		} else {
 			
 			outputGrid[index].coordinate = -1;
+			outputGrid[index].dataAddress = 0;
 		}
 		
 		index += count;
 
 		if (index > (DEPTH-1)) index = -1;
-
 	}
 
 	step <<= 1;
 
-	if (step < 16) 
-	{
-		DeleteRecursion(arr, data, outputGrid, threshold, step);
+	if (step < 16) {
+		DeleteRecursion(arr, data, outputGrid, threshold, step, block);
 	}
 }
 
-
-
-// Delete cells where derivative is below threshold
-void DeleteCells(std::vector<GridVector>& arr, std::vector<DataVector>& data, double threshold)
-{
+// Delete cells in single where derivative is below threshold
+void DeleteVectorCells(std::vector<GridVector>& arr, std::vector<DataVector>& data, int blockID, double threshold){
 	
 	std::vector<GridVector> temp;
 	CreateGridVector(temp,arr.size());
+	temp[0].coordinate = arr.front().coordinate;
+	temp[16].coordinate = arr.back().coordinate;
+	temp[0].dataAddress = arr.front().dataAddress;
+	temp[16].dataAddress = arr.back().dataAddress;
 
-	DeleteRecursion(arr,data,temp,threshold,1);
+	DeleteRecursion(arr,data,temp,threshold,1,blockID);
 	arr.clear();
-	for (int i = 0; i < temp.size(); ++i)
-	{
-		if (temp[i].coordinate != -1)
-		{
+	for (int i = 0; i < DEPTH; ++i){
+		if (temp[i].coordinate != -1){
 			arr.push_back(temp[i]);
 		}
 	}
-	std::vector<DataVector> tempData;
+}
 
-	tempData = data;
-
-	data.clear();
-	for (int i = 0; i < arr.size(); ++i)
-	{
-		data.push_back(tempData[arr[i].dataAddress]);
-		arr[i].dataAddress = i;
-		
+// Delete cells for all vectors in Grid
+void DeleteCells(std::vector<GridBlockVector>& Grid, std::vector<DataVector>& Data, double threshold){
+	for (int i = 0; i < GRIDSIZE; ++i){
+		DeleteVectorCells(Grid[i].GridVector,Data,i,threshold);
 	}
 }
+
+void ReorderData(std::vector<GridBlockVector>& Grid,std::vector<DataVector>& Data){
+
+	std::vector<DataVector> ReorderedData;
+	int counter = 0;
+
+	for (int i = 0; i < GRIDSIZE; ++i){
+		for (int j = 0; j < (Grid[i].GridVector.size() - 1); ++j){
+			int address = Grid[i].GridVector[j].dataAddress;
+			ReorderedData.push_back(Data[address]);
+		}
+	}
+	counter = 0;
+	Data.clear();
+	for(auto it = ReorderedData.begin(); it != ReorderedData.end(); ++it){
+		Data.push_back(*it);
+	}
+	for (int i = 0; i < GRIDSIZE; ++i){
+
+		for (int j = 0; j < (Grid[i].GridVector.size()); j++){
+			if (j < (Grid[i].GridVector.size() - 1)){
+				Grid[i].GridVector[j].dataAddress = counter;
+				++counter;
+			} else {
+				Grid[i].GridVector[j].dataAddress = counter;
+			}
+		}
+	}
+}
+
 
 // Add Cells where derivative is high
 void AddCells(std::vector<GridVector>& arr, std::vector<DataVector>& Data)
@@ -117,45 +162,129 @@ void AddCells(std::vector<GridVector>& arr, std::vector<DataVector>& Data)
 
 }
 
-void PrintVectors(std::vector<GridVector>& arr, std::vector<DataVector>& Data)
-{
-	for (int i = 0; i < arr.size(); ++i)
-	{
+void PrintVectorsSingle(std::vector<GridVector>& arr, std::vector<DataVector>& Data){
+	for (int i = 0; i < arr.size(); ++i){
 		if(arr[i].coordinate < 10) std::cout << '0';
 		std::cout<< arr[i].coordinate << " - ";
 		int iter = arr[i+1].coordinate - arr[i].coordinate;
-		while (iter > 1)
-		{
+		while (iter > 1){
 			std::cout << "** - ";
 			iter--;
-		}
-		
+		}	
 	}
 	std::cout << '\n';
-	for (int i = 0; i < arr.size(); ++i)
-	{
+	for (int i = 0; i < arr.size(); ++i){
 		if(arr[i].dataAddress < 10) std::cout << '0';
 		std::cout<< arr[i].dataAddress << " - ";
 		int iter = arr[i+1].coordinate - arr[i].coordinate;
-		while (iter > 1)
-		{
+		while (iter > 1){
 			std::cout << "** - ";
 			iter--;
 		}
-		
 	}
 	std::cout << '\n';
-	for (int i = 0; i < arr.size(); ++i)
-	{
+	for (int i = 0; i < arr.size(); ++i){
 		if((Data[arr[i].dataAddress].data < 10) && (Data[arr[i].dataAddress].data > -1)) std::cout << '0';
 		std::cout<< Data[arr[i].dataAddress].data << " - ";
 		int iter = arr[i+1].coordinate - arr[i].coordinate;
-		while (iter > 1)
-		{
+		while (iter > 1){
 			std::cout << "** - ";
 			iter--;
 		}
-		
 	}
 	std::cout << '\n';
+}
+
+void PrintGridVectors(std::vector<GridBlockVector>& Grid, std::vector<DataVector>& Data){
+	for (int i = 0; i < GRIDSIZE; ++i){
+		std::cout << i << '\n';
+		for (int j = 0; j < Grid[i].GridVector.size(); ++j){
+			if((Grid[i].GridVector[j].coordinate < 10) 
+				&& (Grid[i].GridVector[j].coordinate != -1))
+				std::cout << '0';
+			if((Grid[i].GridVector[j].coordinate < 100) 
+				&& (Grid[i].GridVector[j].coordinate != -1))
+				std::cout << '0';
+			std::cout << Grid[i].GridVector[j].coordinate << " - ";
+			int iter = Grid[i].GridVector[j+1].coordinate - Grid[i].GridVector[j].coordinate;
+			while (iter > 1){
+				std::cout << "*** - ";
+				iter--;
+			}
+		}
+		std::cout << '\n';
+		for (int j = 0; j < Grid[i].GridVector.size(); ++j){
+			if((Grid[i].GridVector[j].dataAddress < 10) 
+				&& (Grid[i].GridVector[j].dataAddress != -1))
+				std::cout << '0';
+			if((Grid[i].GridVector[j].dataAddress < 100) 
+				&& (Grid[i].GridVector[j].dataAddress != -1))
+				std::cout << '0';
+			std::cout << Grid[i].GridVector[j].dataAddress << " - ";
+			int iter = Grid[i].GridVector[j+1].coordinate - Grid[i].GridVector[j].coordinate;
+			while (iter > 1){
+				std::cout << "*** - ";
+				iter--;
+			}
+		}
+		std::cout << '\n';
+		for (int j = 0; j < Grid[i].GridVector.size(); ++j){
+			double point = Data[Grid[i].GridVector[j].dataAddress].data;
+			if((point < 10)
+				&& (point >= 0))
+				std::cout << '0';
+			if((point < 100)
+				&& (point >= 0))
+				std::cout << '0';
+			if(point < 0)
+				std::cout << ' ';
+
+			std::cout << point << " - ";
+			int iter = Grid[i].GridVector[j+1].coordinate - Grid[i].GridVector[j].coordinate;
+			while (iter > 1){
+				std::cout << "*** - ";
+				iter--;
+			}
+		} std::cout << '\n';
+	}
+}
+
+void PrintGridVectors(std::vector<GridVector>& arr, std::vector<DataVector>& Data, int step){
+	for (int i = 0; i < arr.size(); ++i){
+		if(arr[i].coordinate < 10) std::cout << '0';
+		std::cout<< arr[i].coordinate << " - ";
+		int iter = arr[i+1].coordinate - arr[i].coordinate;
+		while (iter > 1){
+			std::cout << "** - ";
+			iter--;
+		}
+	}
+	std::cout << '\n';
+	for (int i = 0; i < arr.size(); ++i){
+		if(arr[i].dataAddress < 10) std::cout << '0';
+		std::cout<< arr[i].dataAddress << " - ";
+		int iter = arr[i+1].coordinate - arr[i].coordinate;
+		while (iter > 1){
+			std::cout << "** - ";
+			iter--;
+		}
+	}
+	std::cout << '\n';
+	for (int i = 0; i < arr.size(); ++i){
+		int dataAddress = arr[i].dataAddress;
+		if((Data[dataAddress].data < 10) && (Data[dataAddress].data > -1)) std::cout << '0';
+		std::cout<< Data[dataAddress].data << " - ";
+		int iter = arr[i+1].coordinate - arr[i].coordinate;
+		while (iter > 1){
+			std::cout << "** - ";
+			iter--;
+		}
+	} std::cout << '\n';
+}
+
+void GetGridSize(std::vector<GridBlockVector>& Grid, int& size){
+	size = 0;
+	for (int i = 0; i < GRIDSIZE; ++i){
+		size += (Grid[i].GridVector.size() - 1);
+	}
 }
--- WaveSimulation.cpp
+++ WaveSimulation.cpp
@@ -2,28 +2,60 @@
 #include <vector>
 #include "LineFunctions.h"
 
-
-
+void IterateWave(std::ve)
 
 int main()
 {
-	std::vector<GridVector> Grid;
+	std::vector<GridBlockVector> Grid;
 	std::vector<DataVector> Data;
+	CreateDataVector(Data,GRIDSIZE*(DEPTH-1)+1);
+	CreateGridBlockVector(Grid,GRIDSIZE);
 
-	CreateDataVector(Data, DEPTH);
-	CreateGridVector(Grid, DEPTH);
 
-	Data[3].data = -3;
-	Data[4].data = 1;
 
-	Data[15].data = 8;
-	std::cout << "Before Deleting Cells: \n";
-	PrintVectors(Grid,Data);	
+	for (int i = 0; i < DEPTH; ++i)
+	{
+		int jump = i*25;
+		Data[10 + jump].data += -1;
+		Data[11 + jump].data += 4;
+		Data[12 + jump].data += 1;
+		Data[27 + jump].data += 1;
+		Data[28 + jump].data += -1;
+		Data[29 + jump].data += 1;
+	}
+	int counter = 0;
+	for(auto it = Data.begin(); it != Data.end(); ++it)
+	{
+		if( it->data != 0 ) counter++;
+	}	
 
-	DeleteCells(Grid, Data, .01);
+	double threshold = .1;
 
-	std::cout << "\nAfter Deleting Cells: \n";
-	PrintVectors(Grid,Data);
+	
+	int count1 = 0, count2 = 0;
+	GetGridSize(Grid,count1);
+	std::cout << "\n - - - - - - - - - Before Elimination - - - - - - - - - [Grid Size: ";
+	std::cout << count1 << "]\n";
+
+	PrintGridVectors(Grid,Data);
+
+
+
+	DeleteCells(Grid, Data, threshold);
+
+	ReorderData(Grid,Data);
+
+	
+	GetGridSize(Grid,count2);
+	std::cout << "\n - - - - - - - - - After Elimination  - - - - - - - - - [Grid Size: ";
+	std::cout << count2 << "]\n";
+
+	PrintGridVectors(Grid,Data);
+
+	std::cout << "\nCompression Ratio = " << (100 * ((float)count2/(float)count1)) << "%\n";
+
+	std::cout << "\nRetention Ratio = " << (100 * ((float)count2/(float)counter)) << "%\n";
+	
 
 	return 0;
 }
